910035e6d363e428c494d3350d16cc504be7267c
Tanu250906 <tanushreebalakumar@gmail.com>
2025-04-27 16:04:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,10 +35,8 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
               child: Row(
                 children: [
-                  // Search Icon
                   const Icon(Icons.search, color: Colors.black54),
                   const SizedBox(width: 10),
-                  // Simple TextField (no decorations)
                   const Expanded(
                     child: TextField(
                       decoration: InputDecoration(
